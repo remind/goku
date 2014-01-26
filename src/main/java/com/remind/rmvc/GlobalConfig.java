62bb817d15bbc80b1a0680a61acf79eb905ba6f9
remind <408934054@qq.com>
2014-01-26 10:00:22 +0800

    计划所有需要配置的东西都从这个类来取，至于这个类的信息可以从配置文件中获取，会有一个默认的，另外还有一个是用户自定义的配置文件，用户自定义的可以覆盖默认的

---
+++ src/main/java/com/remind/rmvc/GlobalConfig.java
@@ -0,0 +1,17 @@
+package com.remind.rmvc;
+
+/**
+ * 全局配置信息
+ * @author remind
+ *
+ */
+public class GlobalConfig {
+	
+	/**
+	 * controller的包路径
+	 * @return
+	 */
+	public static String getControllerPattern() {
+		return "com.remind.rmvc.test.controller";
+	}
+}
