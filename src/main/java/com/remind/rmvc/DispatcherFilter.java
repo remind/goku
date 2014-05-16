aa6990b96e7e90e68ea038e047ba3ceb79fa0146
remind <408934054@qq.com>
2014-05-16 14:24:25 +0800

    1.重命名DispatcherFilter 2.去掉一个测试框架htmlunit

--- src/main/java/com/remind/rmvc/DispatcherFilter.java
+++ src/main/java/com/remind/rmvc/DispatcherFilter.java
@@ -2,20 +2,18 @@ package com.remind.rmvc;
 
 import java.io.IOException;
 
-import javax.servlet.DispatcherType;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
-import javax.servlet.annotation.WebFilter;
-import javax.servlet.annotation.WebInitParam;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.log4j.Logger;
 
+import com.google.common.base.Strings;
 import com.remind.rmvc.context.HttpContext;
 
 /**
@@ -23,17 +21,18 @@ import com.remind.rmvc.context.HttpContext;
  * @author remind
  *
  */
-@WebFilter(urlPatterns = {"/*"},
-        dispatcherTypes = {DispatcherType.REQUEST},
-        initParams = {@WebInitParam(name = "encoding", value = "UTF-8")}
-)
-public class RmvcDispatcherFilter implements Filter {
+public class DispatcherFilter implements Filter {
 
-	private static Logger logger = Logger.getLogger(RmvcDispatcherFilter.class);
+	private static Logger logger = Logger.getLogger(DispatcherFilter.class);
+	
+	private String encoding = "utf-8";
 	
 	@Override
 	public void init(FilterConfig filterConfig) throws ServletException {
 		logger.info("filter init");
+		if (!Strings.isNullOrEmpty(filterConfig.getInitParameter("encoding"))) {
+			encoding = filterConfig.getInitParameter("encoding");
+		}
 		Application.start();
 	}
 
--- src/test/java/com/remind/rmvc/RouterTest.java
+++ src/test/java/com/remind/rmvc/RouterTest.java
@@ -1,48 +1,7 @@
 package com.remind.rmvc;
 
-import java.io.IOException;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.net.MalformedURLException;
-
-import org.junit.Test;
-
-import com.gargoylesoftware.htmlunit.FailingHttpStatusCodeException;
-import com.gargoylesoftware.htmlunit.WebClient;
-import com.gargoylesoftware.htmlunit.html.HtmlPage;
-import com.remind.rmvc.test.controller.blog.PageController;
 
 public class RouterTest {
 
-	private WebClient webClient = new WebClient();
-
-	public void test() {
-		String url = "http://localhost";
-		try {
-			HtmlPage page = webClient.getPage(url);
-		} catch (FailingHttpStatusCodeException | IOException e) {
-			e.printStackTrace();
-		}
-	}
-
-	@Test
-	public void testRefection() {
-		System.out.println(System.currentTimeMillis());
-		Class<?> cls = PageController.class;
-		
-		Object[] NO_ARGS_OBJECT = new Object[1];
-		Method method;
-		try {
-			Object instance = cls.newInstance();
-			//System.out.println(System.currentTimeMillis());
-			method = cls.getDeclaredMethod("tag", String.class);
-			NO_ARGS_OBJECT[0] = "aa";
-			method.invoke(instance, NO_ARGS_OBJECT);
-		} catch (NoSuchMethodException | SecurityException
-				| IllegalAccessException | IllegalArgumentException
-				| InvocationTargetException | InstantiationException e) {
-			e.printStackTrace();
-		}
-		System.out.println(System.currentTimeMillis());
-	}
+	
 }
