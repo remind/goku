26a7a41364b7dd481074f8542adff3b559123a19
remind <408934054@qq.com>
2014-05-16 15:43:03 +0800

    添加直接输出文本的view

---
+++ src/main/java/com/remind/rmvc/view/impl/TextView.java
@@ -0,0 +1,36 @@
+package com.remind.rmvc.view.impl;
+
+import java.io.IOException;
+import java.io.Writer;
+
+import com.remind.rmvc.context.HttpContext;
+import com.remind.rmvc.internal.ActionResult;
+import com.remind.rmvc.view.View;
+
+/**
+ * 直接输出文本到浏览器
+ * @author remind
+ *
+ */
+public class TextView implements View {
+
+	private HttpContext httpContext = HttpContext.getCurrent();
+	
+	private String content;
+	
+	public TextView(String content) {
+		this.content = content;
+	}
+	
+	@Override
+	public void render(ActionResult actionResult) {
+		try {
+			Writer writer = httpContext.getResponse().getWriter();
+			writer.write(content);
+			writer.flush();
+			writer.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
